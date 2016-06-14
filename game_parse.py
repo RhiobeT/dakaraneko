a8386474acc658a9baa571a97dfef1ee6af4a1af
Nadeflore <flore.love@gmail.com>
2016-06-14 23:43:52 +0200

    Remove random artists generation

--- game_parse.py
+++ game_parse.py
@@ -3,20 +3,11 @@
 #
 
 from anime_parse import parse_file_name as parse_file_name_music
-import random
-
-artists = [
-        "Megurine Luka",
-        "Scandal",
-        "supercell",
-        "Iwasugi Natsu",
-        ]
 
 def parse_file_name(file_name):
     """ From a file name, returns a dictionnary with revelant values 
     """
     result = parse_file_name_music(file_name)
     result['work_type'] = 'Game'
-    result['artists'] = [artists[random.randint(0, 3)] for i in range(random.randint(1, 3))]
 
     return result
