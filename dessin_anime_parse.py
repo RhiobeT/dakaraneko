c4236fd4ee56a7720ac87f1ac47d5a02af4d5508
Nadeflore <flore.love@gmail.com>
2017-03-22 23:43:35 +0100

    Use new nekoparse class for dessin anime parse
    Update karaneko submodule

--- dessin_anime_parse.py
+++ dessin_anime_parse.py
@@ -3,49 +3,53 @@
 # Dakara Project
 #
 
-from nekoparse import cartoon_file2data
+from karaneko.nekoparse import NekoParseCartoon
 from music_parse import extract_tags
 
 def parse_file_name(file_name):
     """ From a file name, returns a dictionnary with revelant values 
     """
     result = {}
-    data = cartoon_file2data(file_name)
-    title = data['title_music']
-    result['title_work'] = data['title_cartoon']
+    cartoon = NekoParseCartoon(file_name)
+    cartoon.parse()
+
+    title = cartoon.title_music
+    result['title_work'] = cartoon.title_cartoon
     if title:
         result['title_music'] = title
     else:
-        result['title_music'] = result['title_work']
-    detail = data['detail']
-    lang = data['language']
+        result['title_music'] = cartoon.title_cartoon
+    detail = cartoon.details
+
+    lang = cartoon.language 
     if detail:
-        result['detail'] = detail + " - " + lang
+            result['detail'] = detail + " - " + lang
     else:
-        result['detail'] = lang
+            result['detail'] = lang
+
+    result['detail_video'] = ""
+    result['version'] = ""
         
-    result['subtitle_work'] = data['subtitle_cartoon']
-    result['work_type_name'] = 'Dessin anime'
+    result['subtitle_work'] = cartoon.subtitle_cartoon or '' 
+    result['work_type_name'] = 'Dessin animé'
     result['work_type_query_name'] = 'dessin-anime'
-    genre = data['genre']
-    if genre['op']:
+
+    tags = cartoon.tags
+    if tags.opening:
         result['link_type'] = 'OP'
-        result['link_nb'] = genre['op_nbr']
-    elif genre['ed']:
+        result['link_nb'] = tags.opening_nbr
+    elif tags.ending:
         result['link_type'] = 'ED'
-        result['link_nb'] = genre['ed_nbr']
-    elif genre['ins']:
+        result['link_nb'] = tags.ending_nbr
+
+    elif tags.insert_song:
         result['link_type'] = 'IN'
-        result['link_nb'] = None 
-    elif genre['is']:
+
+    elif tags.image_song:
         result['link_type'] = 'IS'
-        result['link_nb'] = None 
-    else:
-        result['link_type'] = None
-        result['link_nb'] = None
 
     result['artists'] = []
 
-    result['tags'] = extract_tags(data['genre'])
+    result['tags'] = extract_tags(tags)
 
     return result
